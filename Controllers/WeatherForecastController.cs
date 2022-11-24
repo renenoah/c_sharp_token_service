86a2620597bdb2286fc014990a44481843f295c4
rkouache <rene_noah@hotmail.de>
2022-11-24 21:54:58 +0100

    Änderungen

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -16,11 +16,13 @@ public class WeatherForecastController : ControllerBase
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+        Console.WriteLine("Aufgerufen");
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        _logger.LogInformation("Vorgang erfasst");
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
